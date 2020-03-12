a46b553e703da257e0af942c25be266bf3504baf
Anshuman Mohapatra <amohapatra2001@gmail.com>
2020-03-12 23:36:00 +0530

    Update server.php

--- others/server.php
+++ others/server.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
+require('PHPMailer/PHPMailerAutoload.php'); 
+require('crediantial.php');
 
 // initializing variables
 $name = "";
 $email    = "";
 $regno    ="";
 $username = "";
+$status="";
+$token="";
 $errors = array(); 
 
 // connect to the database
@@ -20,6 +24,8 @@ if (isset($_POST['reg_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $token=(rand('10000','99999'));
+  $status=0;
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
@@ -55,6 +61,42 @@ if (isset($_POST['reg_user'])) {
 
   	$query = "INSERT INTO user (name,email,regno,username, password) 
   			  VALUES('$name','$email','$regno','$username','$password')";
+	  //sending otp
+
+           $last_id = mysqli_insert_id($db);
+           $url='http://'.$_SERVER['SERVER_NAME'].'/send-mail-phpmailer/verify.php?id='.$last_id.'&token='.$token;
+          
+           $output='<div>Thanks for registering.<br>Your OTP is<br>'.$token.'</div>';
+
+
+          if($result==true){
+           $mail = new PHPMailer;
+           $mail->isSMTP();
+           $mail->Host='smtp.gmail.com';
+           $mail->SMTPAuth = true;
+           $mail->Username =EMAIL;
+           $mail->Password =PASS;
+           $mail->SMTPSecure='tls';
+           $mail->Port =587;
+           $mail->setFrom(EMAIL,'ITER ALUMNI TRACKER');
+           $mail->addAddress($email,$name);
+           $mail->isHTML(true);
+         
+
+
+           $mail->Subject = 'Email verification';
+           $mail->Body =$output;
+
+
+            if(!$mail->send()){
+             echo 'Message no sent.';
+             echo 'Mailer Error: '.$mail->ErrorInfo;
+            }else{
+             echo 'Your registration is succesfull, please verify your email.';
+            }
+
+
+          }
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
@@ -90,5 +132,35 @@ if (isset($_POST['login_user'])) {
   	}
   }
 }
+
+
+
+
+
+
+//otp verification
+if(isset($_POST['otp_user'])){
+  $token=mysqli_real_escape_string($db,$_POST['token']);
+
+
+  if(empty($token)){
+    array_push($errors, "Enter the otp"); 
+  }
+  if(count($errors)==0){
+    $querys="SELECT * FROM user WHERE token='$token'";
+    $result1=mysqli_query($db,$querys);
+    if(mysqli_num_rows($result1)==1){
+     $querys1="UPDATE `user` SET   status=1 WHERE token='$_POST[token]'  ";
+     $result2=mysqli_query($db,$querys1);
+     if($result2){
+
+      array_push($errors,"OTP Verified Successfully!!! You can login now ");
+    }
+
+     
+    }else {
+      array_push($errors, "Enter the valid OTP");
+    }
+  }
 
 ?>
