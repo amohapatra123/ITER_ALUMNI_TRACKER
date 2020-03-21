917a472304c57568d15d4eb4b2d1a99943459303
Anshuman Mohapatra <amohapatra2001@gmail.com>
2020-03-21 14:08:21 +0530

    Update server.php

--- others/server.php
+++ others/server.php
@@ -1,15 +1,20 @@
 <?php
 session_start();
-require('PHPMailer/PHPMailerAutoload.php'); 
+//new
+require('../assets/PHPMailer/PHPMailerAutoload.php'); 
 require('crediantial.php');
+//new 
 
 // initializing variables
+$id="";
 $name = "";
 $email    = "";
 $regno    ="";
 $username = "";
-$status="";
-$token="";
+//new 
+ $status="";
+ $token="";
+//new
 $errors = array(); 
 
 // connect to the database
@@ -24,24 +29,25 @@ if (isset($_POST['reg_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  //new
   $token=(rand('10000','99999'));
   $status=0;
+
+  //new 
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Name is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   
-  if (empty($regno)) { array_push($errors, "Uegistration number is required"); }
+  if (empty($regno)) { array_push($errors, "Registration number is required"); }
   if(empty($username)){ array_push($errors, "Username is required");}
   if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
-  }
+  if ($password_1 != $password_2) {array_push($errors, "The two passwords do not match");}
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM user WHERE username='$username'   OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -55,13 +61,18 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
+
+
+ 
+
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO user (name,email,regno,username, password) 
-  			  VALUES('$name','$email','$regno','$username','$password')";
-	  //sending otp
+  	$query = "INSERT INTO user (name,email,regno,username, password,token,status) 
+  			  VALUES('$name','$email','$regno','$username','$password','$token','$status')";
+          //new 
+         //sending otp
 
            $last_id = mysqli_insert_id($db);
            $url='http://'.$_SERVER['SERVER_NAME'].'/send-mail-phpmailer/verify.php?id='.$last_id.'&token='.$token;
@@ -89,7 +100,7 @@ if (isset($_POST['reg_user'])) {
 
 
             if(!$mail->send()){
-             echo 'Message no sent.';
+             echo 'Message not sent.';
              echo 'Mailer Error: '.$mail->ErrorInfo;
             }else{
              echo 'Your registration is succesfull, please verify your email.';
@@ -97,10 +108,11 @@ if (isset($_POST['reg_user'])) {
 
 
           }
+//new
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: profile.php');
+  	header('location: otpverification.php');
   }
 }
 
@@ -111,6 +123,8 @@ if (isset($_POST['reg_user'])) {
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
+  $type=mysqli_real_escape_string($db, $_POST['type']);
+  
 
   if (empty($username)) {
   	array_push($errors, "Username is required");
@@ -121,17 +135,37 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
   	$password = md5($password);
-  	$query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+  	$query = "SELECT * FROM user WHERE username='$username' AND password='$password' AND status=1";
   	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: profile.php');
+  	if (mysqli_num_rows($results)==1) {
+      while($row=mysqli_fetch_assoc($results)){
+
+
+
+        if($row['type']=="Admin"){
+          $_SESSION['username']=$username;
+          header('Location:admin.php');
+
+        }else{
+          $_SESSION['username']=$username;
+          header('Location:profile.php');
+
+        }
+
+      }
+      
+     //  $_SESSION['username'] = $username;
+  	  // $_SESSION['success'] = "You are now logged in";
+  	  // header('location: profile.php');
+  
+    
   	}else {
   		array_push($errors, "Wrong username/password combination");
   	}
   }
 }
+
+
 
 
 
@@ -162,5 +196,28 @@ if(isset($_POST['otp_user'])){
       array_push($errors, "Enter the valid OTP");
     }
   }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+         
+
+
+
+        
+
+
+
+
 
 ?>
