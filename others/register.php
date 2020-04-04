3d86c574b6f5b85fb71db6ec6980465d65859628
Anshuman Mohapatra <amohapatra2001@gmail.com>
2020-04-04 20:28:15 +0530

    Update register.php

--- others/register.php
+++ others/register.php
@@ -1,25 +1,22 @@
  <?php include('server.php') ?>
+ 
  <!DOCTYPE html>
 <html>
 <head>
 	<title>Register</title>
+  <link rel="icon" type="image" href="../assets/images/imgsidebar2.jpg">
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
- <!-- <link rel="stylesheet" type="text/css" href="./assets/css/style.css">-->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="../assets/css/homepage.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	
 	<style>
 		body{
-			background-image: url(../assets/images/imghome9.jpg);
-	    background-repeat: no-repeat;
-	    height:100vh;
-	    width:100%;
-	    background-size:cover;
-	    background-attachment:fixed;
-
+		background-image: url("../assets/images/background.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
 		}
 
     .form-group {
@@ -57,103 +54,116 @@
 			opacity: 0.7; 
 			border-radius: 10px;
 		}
+    .logo{
+        width: 40px;
+        height: 40px;
+      }
+      #header{
+        margin-top: 90px;
+        background-color:whitesmoke;
+      }
+      #formarea{
+        margin-top: 20px;
+        border-radius: 10px;
+        opacity: 0.8;
+      }
 		
 	</style> 
 </head>
 <body >
-  <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center w3-black">
+  <div class="container"  id="navbar">
+    
+    
+       <nav class="navbar navbar-dark  bg-dark navbar-expand-sm fixed-top">
+             <div class="container">
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+               <span class="navbar-toggler-icon"></span> 
+             </button>
+             <a  class="navbar-brand mr-12" href="#"><img src="../assets/images/imgsidebar2.jpg" class="logo"></a>
+               <div class="collapse navbar-collapse" id="Navbar">
+               <ul class="navbar-nav mr-auto">
+               <li class="nav-item"><a class="nav-link " href="../index.html">Home</a></li>
+               <li class="nav-item"><a class="nav-link " href="login.php">Sign In</a></li>
+               <li class="nav-item active"><a class="nav-link " href="register.php">Register</a></li>
+               <li class="nav-item"><a class="nav-link " href="#">Geekcode</a></li>
+               </ul>
+               </div>
+             </div>
+             </nav>
+ </div>
+     <div class="container" id="header">
+      <div class="container" id="heading" style="padding: 10px;">
+        <div class="row">
+          <div class="col-sm-12 col-lg-12" style="text-align: center;background-color:#2196f3;">
+            <h1 style="color:white;font-size: 50px;">Siksha 'O' Anusandhan</h1>
+              </div>
+              <div class="col-sm-12 col-lg-12">
+                <h4 style="text-align: center;">(Deemed to be University)</h4>
+              </div>
+              <div class="col-sm-12 col-lg-12">
+                <b><h3 style="color:green; text-align: center;">ITER Alumni Tracker<br>
+                Institute of Technical Education and Research</h3> </b>
 
-        <a href="https://www.soa.ac.in/iter" target="_blank" class="w3-hover-text-green"><img src="../assets/images/imgsidebar2.jpg" style="width:100%"></a>
-         <a href="homepage.php" class="w3-bar-item w3-button w3-padding-large w3-black">
-            <i class="fa fa-home w3-xxlarge"></i>
-            <p>HOME</p>
-        </a>
-
-        <a href="login.php" class="w3-bar-item w3-button w3-padding-large w3-black">
-            <i class="fa fa-sign-in w3-xxlarge"></i>
-            <p>SIGN IN</p>
-        </a>
-
-       <a href="register.php" class="w3-bar-item w3-button w3-padding-large w3-black">
-            <i class="fa fa-pencil-square w3-xxlarge"></i>
-            <p>REGISTER</p>
-        </a>
-
-       
-
-        <a href="https://github.com/Sudeep25022000/Alumini-System-Hackerwar2020-" target="_blank" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-            <i class="fa fa-code w3-xxlarge"></i>
-            <p>GEEKCODE</p>
-        </a>
-    </nav>
+              </div>
+          </div>
+      </div>
     </div>
-  <div class="w3-white" >
-  <div class="w3-padding-large" id="main">
-        <!-- Header/Home -->
-        <header class="w3-container w3-padding-32 w3-center w3-light-grey" id="home">
-
-            <h3 class="w3-jumbo w3-blue ">Siksha 'O' Anusandhan<br>
-                <h4 class="w3-text-black w3-light-grey ">(Deemed to be University)</h4>
-            </h3>
-            <p class="w3-text-green w3-arial"><b><h3 style="color:green">ITER Alumni Tracker<br>Institute of Technical Education and Research</h3> </b></p>
-	  </header>
-  </div>
-</div>
-
-    <div class="upperbackground">
-     <center>
-    <div class="me" >
-    	<label style="color:white;font-size: 30px;">Register Here....</label>
-    <form  method="POST" action="register.php">
-    	 <?php include('errors.php'); ?>
-  <div class="form-group">
-    <label style="color:white;font-size:25px;">Name</label>
-    <input type="text" class="form-control" name="name" placeholder="Enter Name" value="<?php echo $name; ?>" >
-   
-  </div>
-  <div class="form-group">
-    <label  style="color:white;font-size:25px;">Email</label>
-    <input type="email" class="form-control" name="email" placeholder="Enter Email"  value="<?php echo $email; ?>" >
-   
-  </div>
-  <div class="form-group">
-    <label  style="color:white;font-size:25px;">Registration Number</label>
-    <input type="text" class="form-control" name="regno" placeholder="Enter University Registration Number" value="<?php echo $regno; ?>" >
-  </div>
-  
- 
-   <div class="form-group">
-    <label  style="color:white;font-size:25px;">Username</label>
-    <input type="text" class="form-control" name="username" placeholder="Enter Username" value="<?php echo $username; ?>" >
-  </div>
-   <div class="form-group">
-    <label  style="color:white;font-size:25px;">Password</label>
-    <input type="password" class="form-control" name="password_1" placeholder="Enter Password" >
-  </div>
-   <div class="form-group">
-    <label  style="color:white;font-size:25px;">Confirm Password</label>
-    <input type="password" class="form-control" name="password_2" placeholder="Confirm Password">
-  </div>
-  <input type="submit" class="btn btn-primary"   name="reg_user" value="Submit" onclick="myfunction()">
-  <p style="color:white;">
-      Already a member? <a href="login.php">Sign in</a>
-    </p>
-</form>
-    		
-</div>
-
-</center>
-
+    <div class="container"  style="background-color: black;" id="formarea">
+      <div class="row">
+        <div class="col-md-4  offset-md-4">
+          <label><h1 style="color: white;">Register Here!!!</h1></label>
+        </div>
+      </div>
+      <form method="POST" action="register.php">
+        <div class="form-group row">
+        <div class="col-md-10 offset-md-1">
+        <?php include('errors.php'); ?>
+        </div>
+        </div>
+        <div class="form-group row">
+          <label for="name" class="col-md-2 col-form-label offset-md-2" style="color:white;">Name</label>
+          <div class="col-md-6"><input type="text" class="form-control" name="name" id="name" placeholder="Enter Name"   value="<?php echo $name; ?>" ></div>
+        </div>
+         <div class="form-group row">
+          <label for="email" class="col-md-2 col-form-label offset-md-2" style="color:white;">Email</label>
+          <div class="col-md-6"><input type="email" class="form-control" name="email"   id="email" placeholder="Enter Email"   value="<?php echo $email; ?>" ></div>
+        </div>
+         <div class="form-group row">
+          <label for="regno" class="col-md-2 col-form-label offset-md-2" style="color:white;">Registration Number</label>
+          <div class="col-md-6"><input type="text" class="form-control" name="regno"   id="regno" placeholder="Enter University Registration Number"   value="<?php echo $regno; ?>" ></div>
+        </div>
+         <div class="form-group row">
+          <label for="username" class="col-md-2 col-form-label offset-md-2" style="color:white;">Username</label>
+          <div class="col-md-6"><input type="text" class="form-control" name="username"   id="username" placeholder="Enter Username"   value="<?php echo $username; ?>" ></div>
+        </div>
+         <div class="form-group row">
+          <label for="password_1" class="col-md-2 col-form-label offset-md-2" style="color:white;">Password</label>
+          <div class="col-md-6"><input type="password" class="form-control" name="password_1"   id="password_1" placeholder="Enter Password"></div>
+        </div>
+        <div class="form-group row">
+          <label for="password_2" class="col-md-2 col-form-label offset-md-2" style="color:white;">Confirm Password</label>
+          <div class="col-md-6"><input type="password" class="form-control" name="password_2"   id="password_2" placeholder="Confirm Password"></div>
+        </div>
+        <div class="form-group row">
+          <div class="col-md-2 offset-md-2">
+            <input type="submit" class="btn btn-primary"   name="reg_user" value="Submit" onclick="myfunction()">
+            <p style="color:white;">
+            Already a member? <a href="login.php">Sign in</a>
+            </p>
+          </div>
+          
+        </div>
+      </form>
     </div>
-
 
 </body>
 </html>
-
-
-<script src="js/aos.js"></script>
+<script src="../assets/js/aos.js"></script>
 <script>
   AOS.init({
     easing: 'ease-in-out-sine'
   });
 </script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
